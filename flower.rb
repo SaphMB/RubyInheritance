d05db9de2852727c2429fe523a5ee11f72ed1edb
Sapphire <saphmb@gmail.com>
2017-05-06 18:12:30 +0100

    added constants

--- flower.rb
+++ flower.rb
@@ -6,9 +6,22 @@ class Flower
 		@color = color
 	end
 
+	ROSE = "red"
+	ANEMONE = "purple"
+	CHRYSANTHEMUM = "yellow"
+
 	class << self
 		def with_red_color
 			Flower.new("red")
 		end
+	end
 
 end
+
+molly = Flower.new("orange")
+puts molly
+
+lolly = Flower.with_red_color
+polly = Flower.new(Flower::ROSE)
+dolly = Flower.new(Flower::CHRYSANTHEMUM)
+holly = Flower.new(Flower::ANEMONE)
